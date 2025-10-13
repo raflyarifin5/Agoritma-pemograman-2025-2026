4f32949fac7e32e78371513413bf98e3abb37a60
raflyarifin5 <raflyarifin5@gmail.com>
2025-10-13 11:11:11 +0700

    belajar & coba

---
+++ C++/latihan.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string nama = "rafly";
+    int umur = 19;
+    float tinggi = 170.5;
+    bool aktif = true;
+
+    cout << "Nama: " << nama << endl;
+    cout << "Umur: " << umur << endl;
+    cout << "Tinggi: " << tinggi << endl;
+    cout << "Status aktif: " << aktif << endl;
+
+    return 0;
+}
